2043bd5bb680884601ecc29086ca04a83b5d9476
Mary <mary.sk2000@gmail.com>
2023-07-10 22:48:31 +0300

    add some style for one book page

--- src/Components/OneBookPage.tsx
+++ src/Components/OneBookPage.tsx
@@ -30,41 +30,45 @@ export const OneBookPage = () => {
       navigate(-1)
    }
 
-   if (!book.title) return (<h2>Book not found</h2>)
+   if (!book.title) return (<Typography variant="h1" component='h1'>Book not found</Typography>)
 
-   return (<Box>
-      <Button onClick={handleClickBack}>
-         <KeyboardBackspaceIcon fontSize="large" />
-      </Button>
-      <Typography variant="h1" component='h1'>{book.title}</Typography>
+   return (
       <Box sx={{
-         display: 'flex',
-         flexDirection: 'row',
-         justifyContent: 'space-between',
-         paddingX: '10px',
-         maxWidth: '1200px',
+         maxWidth: '1120px',
          marginX: 'auto',
          marginY: '0',
-         paddingY: '20px'
+         paddingX: '40px'
       }}>
-         <Card>
-            <CardMedia
-               component="img"
-               // height="100%"
-               image={book.image}
-               alt="book"
-            // sx={{ maxWidth: 0.8 }}
-            />
-         </Card>
-         <Card sx={{ width: '450px' }}>
-            <CardContent>
-               {/* <Typography gutterBottom variant="h3" component="h3">
+         <Button onClick={handleClickBack} sx={{ mt: '70px' }}>
+            <KeyboardBackspaceIcon fontSize="large" sx={{ color: "system.main" }} />
+         </Button>
+
+         <Typography variant="h1" component='h1' sx={{ pt: '30px', pb: '50px' }}>{book.title}</Typography>
+
+         <Box sx={{
+            display: 'flex',
+            justifyContent: 'space-between'
+         }}>
+            <Card
+               // sx={{ width: '545px' }}
+               sx={{ bgcolor: 'tertiary2.light', width: '440px' }}
+            >
+               <CardMedia
+                  component="img"
+                  image={book.image}
+                  alt="book"
+                  sx={{ width: '80%', marginX: 'auto', marginY: '0' }}
+               />
+            </Card>
+            <Card sx={{ width: '450px' }}>
+               <CardContent>
+                  {/* <Typography gutterBottom variant="h3" component="h3">
                   {book.title}
                </Typography>
                <Typography variant="body1" color="system.light">
                   {book.subtitle}
                </Typography> */}
-               {/* <TableContainer>
+                  {/* <TableContainer>
                   <Table aria-label="book props table">
                      <TableBody>
                         <TableRow >
@@ -74,57 +78,55 @@ export const OneBookPage = () => {
                      </TableBody>
                   </Table>
                </TableContainer> */}
-               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Typography gutterBottom variant="h2" component="h2">
-                     {book.price}
-                  </Typography>
-                  <Rating value={Number(book.rating)}
-                     sx={{
-                        pr: '10px',
-                        alignSelf: 'center'
-                        // fontSize: 40
-                     }} />
-               </Box>
-               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Typography gutterBottom variant="body1" component="p" sx={{ color: 'system.light' }}>
-                     Authors
-                  </Typography>
-                  <Typography maxWidth='125px' gutterBottom variant="body1" component="p">
-                     {book.authors}
-                  </Typography>
-               </Box>
-               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Typography gutterBottom variant="body1" component="p" sx={{ color: 'system.light' }}>
-                     Publisher
-                  </Typography>
-                  <Typography maxWidth='125px' gutterBottom variant="body1" component="p">
-                     {book.publisher}
-                  </Typography>
-               </Box>
-               <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
-                  <Typography gutterBottom variant="body1" component="p" sx={{ color: 'system.light' }}>
-                     Language
-                  </Typography>
-                  <Typography maxWidth='125px' gutterBottom variant="body1" component="p">
-                     {book.language}
-                  </Typography>
-               </Box>
-            </CardContent>
-         </Card>
-
-      </Box>
-      {/* <Box sx={{ width: '300px' }}></Box> */}
-      <TabContext value={value}>
-         <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-            <TabList aria-label="Tabs for book props" onChange={handleChange}>
-               <Tab label='Description' value='1' />
-               <Tab label='Authors' value='2' />
-               {/* <Tab label='Reviews' value='3' /> */}
-            </TabList>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                     <Typography gutterBottom variant="h2" component="h2">
+                        {book.price}
+                     </Typography>
+                     <Rating value={Number(book.rating)}
+                        sx={{
+                           pr: '10px',
+                           alignSelf: 'center'
+                           // fontSize: '40px'
+                        }} />
+                  </Box>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                     <Typography gutterBottom variant="body1" component="p" sx={{ color: 'system.light' }}>
+                        Authors
+                     </Typography>
+                     <Typography maxWidth='125px' gutterBottom variant="body1" component="p">
+                        {book.authors}
+                     </Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                     <Typography gutterBottom variant="body1" component="p" sx={{ color: 'system.light' }}>
+                        Publisher
+                     </Typography>
+                     <Typography maxWidth='125px' gutterBottom variant="body1" component="p">
+                        {book.publisher}
+                     </Typography>
+                  </Box>
+                  <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                     <Typography gutterBottom variant="body1" component="p" sx={{ color: 'system.light' }}>
+                        Language
+                     </Typography>
+                     <Typography maxWidth='125px' gutterBottom variant="body1" component="p">
+                        {book.language}
+                     </Typography>
+                  </Box>
+               </CardContent>
+            </Card>
          </Box>
-         <TabPanel value='1'>{book.desc}</TabPanel>
-         <TabPanel value='2'>{book.authors}</TabPanel>
-         {/* <TabPanel value='3'>{book.publisher}</TabPanel> */}
-      </TabContext>
-   </Box>)
+
+         <TabContext value={value}>
+            <Box sx={{ borderBottom: 1, borderColor: 'divider', pt: '70px' }}>
+               <TabList aria-label="Tabs for book props" onChange={handleChange} textColor="secondary" indicatorColor="secondary" >
+                  <Tab sx={{ color: 'system.light', fontSize: '16px', textTransform: 'capitalize' }} label='Description' value='1' />
+                  <Tab sx={{ color: 'system.light', fontSize: '16px', textTransform: 'capitalize' }} label='Authors' value='2' />
+                  {/* <Tab label='Reviews' value='3' /> */}
+               </TabList>
+            </Box>
+            <TabPanel value='1' sx={{ pt: '50px', pb: '70px', fontSize: '16px' }}>{book.desc}</TabPanel>
+            <TabPanel value='2' sx={{ pt: '50px', pb: '70px', fontSize: '16px' }}>{book.authors}</TabPanel>
+         </TabContext>
+      </Box>)
 }
